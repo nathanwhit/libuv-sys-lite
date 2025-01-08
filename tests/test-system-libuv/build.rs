1821462071309fb0da9bc9ec269e953e5ca2cc51
Nathan Whitaker <nathan@deno.com>
2025-01-07 22:11:55 -0800

    this time

--- tests/test-system-libuv/build.rs
+++ tests/test-system-libuv/build.rs
@@ -1,7 +1,9 @@
 fn main() {
   if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
     if let Ok(link_path) = std::env::var("LIBUV_LIB_PATH") {
-      println!("cargo:rustc-link-search=native={}", link_path);
+      for path in link_path.split(';') {
+        println!("cargo:rustc-link-search={}", path);
+      }
       println!("cargo:rustc-link-lib=uv");
       if let Ok(include_path) = std::env::var("LIBUV_INCLUDE_PATH") {
         println!("cargo:include={}", include_path);
